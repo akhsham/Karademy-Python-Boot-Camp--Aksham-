d49c29cd19631a90359a780678295a3b492914d0
Akhsham F. Darian <darian@Akshams-MBP.lan>
2020-07-30 09:53:11 +0430

    Add finished old programs

--- week1/find_the_letter/find-the-letter.py
+++ week1/find_the_letter/find-the-letter.py
@@ -9,6 +9,7 @@ user_list = input("please write your list according to this format \n"
                   "[a,b,c,d,e,f]\n"
                       "::  \n")
 user_sample = input("sample: ")
+
 for letter in user_sample:
     if letter in user_list:
         print(letter)
